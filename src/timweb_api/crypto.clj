9e95a1ed7d13fdfa38dd2905c0658bc3018e6da7
Denis Rodionov <denis.rodionov@aibidia.com>
2020-07-19 20:16:37 +0300

    Trying some spec

--- src/timweb_api/crypto.clj
+++ src/timweb_api/crypto.clj
@@ -1,5 +1,6 @@
 (ns timweb-api.crypto
   (:require [buddy.hashers :as hashers]
+            [clojure.spec.alpha :as s]
             [tick.alpha.api :as t]
             [buddy.core.nonce :as nonce]
             [buddy.auth.backends.token :refer [jws-backend]]
@@ -15,8 +16,9 @@
   "Time when token is supposed to expire"
   (t/+ (t/now) (t/new-duration (:token-longevity server-config) :seconds)))
 
-(defn hash-password [password]
+(defn hash-password
   "Hashes the password to bcrypt+sha512"
+  [password]
   (hashers/derive password))
 
 (defn check-password [hashed-password plain-password]
@@ -28,3 +30,11 @@
   (let [claims {:user (keyword login)
                 :exp (get-token-expiration)}]
     (jwt/sign claims secret {:alg :hs512})))
+
+(s/fdef hash-password
+        :args (s/cat :password string?)
+        :ret string?)
+
+(s/fdef check-password
+        :args (s/cat :hashed-password string? :plain-password string?)
+        :ret boolean?)
