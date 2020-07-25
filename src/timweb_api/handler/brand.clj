e5729088dc59717229544721e8ed461e0153d7a1
Denis Rodionov <denis.rodionov@aibidia.com>
2020-07-26 02:06:53 +0300

    Fixed brands counter cache

--- src/timweb_api/handler/brand.clj
+++ src/timweb_api/handler/brand.clj
@@ -16,8 +16,9 @@
 ;; returns full brand with id
 (defn add-brand
   [{body :body-params}]
-  (cache/update-brands-count)
-  (ok (first (db/add-brand body))))
+  (let [new-brand (first (db/add-brand body))]
+    (cache/update-brands-count)
+    (ok new-brand)))
 
 (defn update-brand
   [{body :body-params path :path-params}]
@@ -32,12 +33,12 @@
 
 (defn delete-brand
   [{path :path-params}]
-  (cache/update-brands-count)
   (let [number-of-deleted-rows (->> :brand-id
                                     (get path)
                                     (Integer/parseInt)
                                     (db/delete-brand)
                                     (first))]
+    (cache/update-brands-count)
     (if (> number-of-deleted-rows 0)
       (ok)
       (bad-request "Not deleted. Possibly wrong brand id"))))
