0a667f0493695f69417ed3f75d7b7674abe2f820
Denis Rodionov <denis.rodionov@aibidia.com>
2020-08-03 00:40:41 +0300

    menu

--- src/timweb_api/template/partials.clj
+++ src/timweb_api/template/partials.clj
@@ -3,13 +3,29 @@
             [hiccup.core :refer [html]]
             [timweb-api.template.styles :as styles]))
 
+(defn menu []
+  (html [:nav.menu
+         [:ul
+          [:li "Расписание"]
+          [:li "Компьютерные симуляции"]
+          [:li "Семинары"]
+          [:li "Статистика"]
+          [:li "Клиенты"]
+          [:li "Отзывы"]
+          [:li "Контакты"]]]))
+
 (defn header []
-  (html [:header "asdasd"]))
+  (html [:header
+         [:div.logo
+          [:img {:src "assets/img/logo.svg"}]
+          [:div.title "Группа ТИМ"]]
+         (menu)]))
 
 (defn application [title & content]
   (html5 [:head
           [:title title]
           (include-css "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css")
+          (include-css "assets/reset.css")
           (include-css "assets/styles.css")
           [:body
            (header)
--- src/timweb_api/template/styles.clj
+++ src/timweb_api/template/styles.clj
@@ -3,12 +3,31 @@
 
 (def target-css-name "resources/public/styles.css")
 
+(def menu
+  [:.menu [:ul {:display "flex"}
+           [:li {:margin "0 1rem"}]]])
+
+(def logo
+  [:div.logo {:display "flex"}
+   [:img {:margin "0 1rem 0 1rem"}]
+   [:.title {:position "relative"
+             :top "2px"}]])
+
 (def header
-  [:header {:background-color "red"
-            :width "200px"
-            :height "100px"}])
+  [:header {:position "sticky"
+            :background-color "white"
+            :top "0"
+            :display "flex"
+            :align-items "center"
+            :justify-content "space-between"
+            :height "4rem"
+            :width "100%"}
+   logo
+   menu])
+
+
 
 (defn compile-styles
   "Compiles all styles using garden into a single css file"
   []
-  (css {:output-to target-css-name} header))
+  (css {:output-to target-css-name} (conj [] header)))
